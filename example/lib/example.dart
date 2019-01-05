24f83ebf414aa79f5cfc5700b8d928b1e3396ff7
Mathieu Post <mathieupost@gmail.com>
2019-01-05 17:08:45 +0100

    Add infinite loop example

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -31,31 +31,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIntValue = 10;
+  int _currentInfIntValue = 10;
   double _currentDoubleValue = 3.0;
   NumberPicker integerNumberPicker;
+  NumberPicker integerInfiniteNumberPicker;
   NumberPicker decimalNumberPicker;
-
-  _handleValueChanged(num value) {
-    if (value != null) {
-      if (value is int) {
-        setState(() => _currentIntValue = value);
-      } else {
-        setState(() => _currentDoubleValue = value);
-      }
-    }
-  }
-
-  _handleValueChangedExternally(num value) {
-    if (value != null) {
-      if (value is int) {
-        setState(() => _currentIntValue = value);
-        integerNumberPicker.animateInt(value);
-      } else {
-        setState(() => _currentDoubleValue = value);
-        decimalNumberPicker.animateDecimalAndInteger(value);
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,36 +44,61 @@ class _MyHomePageState extends State<MyHomePage> {
       minValue: 0,
       maxValue: 100,
       step: 10,
+      onChanged: (value) => setState(() => _currentIntValue = value),
+    );
+    integerInfiniteNumberPicker = new NumberPicker.integer(
+      initialValue: _currentInfIntValue,
+      minValue: 0,
+      maxValue: 99,
+      step: 10,
       infiniteLoop: true,
-      onChanged: _handleValueChanged,
+      onChanged: (value) => setState(() => _currentInfIntValue = value),
     );
     decimalNumberPicker = new NumberPicker.decimal(
-        initialValue: _currentDoubleValue,
-        minValue: 1,
-        maxValue: 5,
-        decimalPlaces: 2,
-        onChanged: _handleValueChanged);
+      initialValue: _currentDoubleValue,
+      minValue: 1,
+      maxValue: 5,
+      decimalPlaces: 2,
+      onChanged: (value) => setState(() => _currentDoubleValue = value),
+    );
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text(widget.title),
-        ),
-        body: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              integerNumberPicker,
-              new RaisedButton(
-                onPressed: () => _showIntDialog(),
-                child: new Text("Current int value: $_currentIntValue"),
-              ),
-              decimalNumberPicker,
-              new RaisedButton(
-                onPressed: () => _showDoubleDialog(),
-                child: new Text("Current double value: $_currentDoubleValue"),
-              ),
-            ],
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          new Row(children: <Widget>[
+            Expanded(
+              child: Column(children: <Widget>[
+                Text("Default"),
+                integerNumberPicker,
+                new RaisedButton(
+                  onPressed: () => _showIntDialog(),
+                  child: new Text("Current int value: $_currentIntValue"),
+                ),
+              ]),
+            ),
+            Expanded(
+              child: Column(children: <Widget>[
+                Text("With infinite loop"),
+                integerInfiniteNumberPicker,
+                new RaisedButton(
+                  onPressed: () => _showInfIntDialog(),
+                  child: new Text("Current int value: $_currentInfIntValue"),
+                ),
+              ]),
+            ),
+          ]),
+          decimalNumberPicker,
+          new RaisedButton(
+            onPressed: () => _showDoubleDialog(),
+            child: new Text("Current double value: $_currentDoubleValue"),
           ),
-        ));
+        ],
+      ),
+    );
   }
 
   Future _showIntDialog() async {
@@ -107,7 +112,32 @@ class _MyHomePageState extends State<MyHomePage> {
           initialIntegerValue: _currentIntValue,
         );
       },
-    ).then(_handleValueChangedExternally);
+    ).then((num value) {
+      if (value != null) {
+        setState(() => _currentIntValue = value);
+        integerNumberPicker.animateInt(value);
+      }
+    });
+  }
+
+  Future _showInfIntDialog() async {
+    await showDialog<int>(
+      context: context,
+      builder: (BuildContext context) {
+        return new NumberPickerDialog.integer(
+          minValue: 0,
+          maxValue: 99,
+          step: 10,
+          initialIntegerValue: _currentInfIntValue,
+          infiniteLoop: true,
+        );
+      },
+    ).then((num value) {
+      if (value != null) {
+        setState(() => _currentInfIntValue = value);
+        integerInfiniteNumberPicker.animateInt(value);
+      }
+    });
   }
 
   Future _showDoubleDialog() async {
@@ -122,6 +152,11 @@ class _MyHomePageState extends State<MyHomePage> {
           title: new Text("Pick a decimal number"),
         );
       },
-    ).then(_handleValueChangedExternally);
+    ).then((num value) {
+      if (value != null) {
+        setState(() => _currentDoubleValue = value);
+        decimalNumberPicker.animateInt(value);
+      }
+    });
   }
 }
